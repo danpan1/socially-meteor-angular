6bcdffc136dd86c5fa678c0b4dba9a00c960c4ab
Pankrashin Danil <danpan@yandex.ru>
2016-03-11 13:03:58 +0300

    start step 12

---
+++ client/lib/app.js
@@ -0,0 +1,19 @@
+angular.module('socially', [
+  'angular-meteor',
+  'ui.router',
+  'accounts.ui'
+]);
+
+function onReady() {
+  angular.bootstrap(document, ['socially'], {
+    strictDi: true
+  });
+}
+
+if (Meteor.isCordova)
+  angular.element(document)
+  .on("deviceready", onReady);
+else
+  angular.element(document)
+  .ready(onReady);
+
